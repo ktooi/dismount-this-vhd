8bf5da35d41fca317cefe0944ca72bb51bfaced5
Kodai Tooi <26849015+ktooi@users.noreply.github.com>
2021-03-06 02:04:47 +0900

    Update dismount-this-vhd.ps1

--- dismount-this-vhd.ps1
+++ dismount-this-vhd.ps1
@@ -1,2 +1,4 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+# FYI: https://correct-log.com/powershell_auto_admin/
+
 Get-Volume | where DriveLetter -eq $(Split-Path -Qualifier $PSCommandPath).Replace(":","") | Get-Partition | Dismount-VHD
